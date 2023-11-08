e86fabffdbf66195e479275449eecc6bfff0d344
nagee <nagee@Nageena>
2023-11-08 09:37:05 +0530

    first commit

---
+++ src/com/lbrce/team/Projectdemo.java
@@ -0,0 +1,16 @@
+package com.lbrce.team;
+
+public class Projectdemo 
+{
+	public static void main(String args[]) {
+	int n=12;
+	if(n%2==0) 
+	{
+		System.out.println("even");
+	}
+	else 
+	{
+		System.out.println("odd");
+	}
+}
+}
---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module TeamLeadProject1 {
+}
